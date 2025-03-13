9064903d28936997af074a4eb55d801242159f94
Natalie Blumen <natalie.blumen@gmail.com>
2025-03-13 11:07:42 -0400

    mar13

--- react_tutorial/src/Profile.jsx
+++ react_tutorial/src/Profile.jsx
@@ -8,19 +8,59 @@ const person = {
   }
 }
 
+const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',  
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+}];
+
+const chemists = people.filter(person => 
+  person.profession === "chemist"
+);
+
+const listItems = chemists.map(person => 
+  <li>
+    <p>
+      <b>{person.name}</b>
+      {' ' + person.profession + ' '}
+      known for {person.profession}
+    </p>
+  </li>
+);
+
 function Avatar(props){
 
   let person = props.person;
   let size = props.size;
 
   return (
-    <img 
-      className="avatar"
-      src={getImageUrl(person)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
+    <>
+      <img 
+        className="avatar"
+        src={getImageUrl(person)}
+        alt={person.name}
+        width={size}
+        height={size}
+      />
+      <ul>{listItems}</ul>
+    </>
+    
   );
 }
 
